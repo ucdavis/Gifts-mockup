206b5bd041899f3723534fce9aca220050bf724f
Scott Kirkland <srkirkland@gmail.com>
2014-03-10 12:09:41 -0700

    navigation and default homepage

--- Gifts/App_Start/RouteConfig.cs
+++ Gifts/App_Start/RouteConfig.cs
@@ -16,7 +16,7 @@ namespace Gifts
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "Mockup", action = "Index", id = UrlParameter.Optional }
             );
         }
     }
